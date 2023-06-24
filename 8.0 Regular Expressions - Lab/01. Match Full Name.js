0132078e5559e368b770ab5b4227b36216918a8e
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-06-24 18:42:41 +0300

    Add files via upload

---
+++ 8.0 Regular Expressions - Lab/01. Match Full Name.js
@@ -0,0 +1,5 @@
+str => String(str).match(/\b[A-Z][a-z]+ [A-Z][a-z]+\b/g).join(' ')
+
+//s = 
+//console.log(s(["Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan	Ivanov"]))
+
---
+++ 8.0 Regular Expressions - Lab/03. Match Dates.js
@@ -0,0 +1,27 @@
+str => str[0].match(/\d{2}([\./-])[A-Z]\w{2}\1\d{4}/gm)
+  .forEach(el => console.log(`Day: ${el.slice(0, 2)}, Month: ${el.slice(3, 6)}, Year: ${el.slice(-4)}`))
+
+//----------------------------------------(2)---------------------------------------------
+
+function matchDates(input) {
+  let pattern = input[0].match(/\d{2}([\./-])[A-Z]\w{2}\1\d{4}/gm)
+  for (const el of pattern) 
+    console.log(`Day: ${el.slice(0, 2)}, Month: ${el.slice(3, 6)}, Year: ${el.slice(-4)}`)
+}
+
+    
+// matchDates(["13/Jul/1928, 10-Nov-1934, , 01/Jan-1951,f 25.Dec.1937 23/09/1973, 1/Feb/2016"])
+
+//----------------------------------------(3)----------------------------
+function matchDates(input) {
+  let result = input[0].match(/\d{2}([\./-])[A-Z]\w{2}\1\d{4}/gm)
+
+  for (const el of result) {
+    let day = el.slice(0, 2)
+    let month = el.slice(3, 6)
+    let year = el.slice(-4)
+    console.log(`Day: ${day}, Month: ${month}, Year: ${year}`);
+  }
+}
+
+// matchDates(["13/Jul/1928, 10-Nov-1934, , 01/Jan-1951,f 25.Dec.1937 23/09/1973, 1/Feb/2016"])
