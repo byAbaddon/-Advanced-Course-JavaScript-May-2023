97eb91ea11750b1a4fd3d05f740f0aea04ccb12d
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-06-29 15:30:34 +0300

    Update 06. Word Occurrences.js

--- 6.0 Associative Arrays - Lab/06. Word Occurrences.js
+++ 6.0 Associative Arrays - Lab/06. Word Occurrences.js
@@ -4,27 +4,17 @@ arr => Object.entries(arr.reduce((acc, x) => (!acc[x] ? acc[x] = 1 : acc[x]++, a
 
 //-------------------------------------(2)----------------------
 
-function wordOccurrences(inputArr) {
-  let arr = [...inputArr]
+function wordOccurrences(arg) {
   let map = new Map()
 
-  while (arr.length > 0) {
-    let word = arr.shift()
-
-    if (!map.has(word)) {
-      map.set(word, 1)
-    } else {
-      let value = map.get(word)
-      map.set(word, value + 1)
-    }
+  while (arg.length) {
+    let word = arg.shift()
+    if (!map.has(word)) map.set(word, 1)
+    else   map.set(word, map.get(word) + 1)
   }
 
-  let sortValue = [...map.entries()].sort((a, b) => b[1] - a[1])
-
-  for (const key of sortValue) {
-    console.log(key[0] + ' -> ' + key[1] + ' times')
-  }
-
+  [...map.entries()].sort((a, b) => b[1] - a[1])
+    .forEach(key => console.log(key[0] + ' -> ' + key[1] + ' times'))
 }
 
 //wordOccurrences(["Here", "is", "the", "first", "sentence", "Here", "is", "another", "sentence", "And", "finally", "the", "third", "sentence"])
