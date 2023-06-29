6152a3c3c1a6520a15833f5e34e7a948a3ff0234
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-06-29 16:38:39 +0300

    Update 06. Word Occurrences.js

--- 6.0 Associative Arrays - Lab/06. Word Occurrences.js
+++ 6.0 Associative Arrays - Lab/06. Word Occurrences.js
@@ -4,7 +4,7 @@ arg => Object.entries(arg.reduce((a, x) => (a[x] = (a[x] || 0) + 1, a), {}))
 
 //--------------------------------------------------------------//reduce
 
-arr => Object.entries(arr.reduce((acc, x) => (!acc[x] ? acc[x] = 1 : acc[x]++, acc), {}))
+arg => Object.entries(arg.reduce((a, x) => (a[x] ? a[x]++ : a[x] = 1, a), {}))
   .sort((a, b) => b[1] - a[1])
   .forEach(x => console.log(x[0], '->', x[1], 'times'))
 
