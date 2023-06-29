51e54c3c55e43f256a410137038df9f9435faca5
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-06-29 15:55:35 +0300

    Update 06. Word Occurrences.js

--- 6.0 Associative Arrays - Lab/06. Word Occurrences.js
+++ 6.0 Associative Arrays - Lab/06. Word Occurrences.js
@@ -1,8 +1,28 @@
+//--------------------------------------------------------------//reduce
+
 arr => Object.entries(arr.reduce((acc, x) => (!acc[x] ? acc[x] = 1 : acc[x]++, acc), {}))
   .sort((a, b) => b[1] - a[1])
   .forEach(x => console.log(x[0], '->', x[1], 'times'))
 
-//-------------------------------------(2)----------------------
+//-------------------------------------(2)-----------------------//obj short
+function wordOccurrences(arg, obj={}) {
+  for (word of arg) obj[word] = (obj[word] || 0) + 1
+  Object.entries(obj).sort((a, b) => b[1] - a[1])
+    .forEach(x => console.log(x[0] + ' -> ' + x[1] + ' times'))
+}
+
+//-------------------------------------(3)----------------------//obj
+function wordOccurrences(arg, obj={}) {
+  for (word of arg) {
+    if(!obj[word]) obj[word] = 0
+    obj[word] += 1
+  }
+
+  Object.entries(obj).sort((a, b) => b[1] - a[1])
+    .forEach(x => console.log(x[0] + ' -> ' + x[1] + ' times'))
+}
+
+//-------------------------------------(4)----------------------//map
 
 function wordOccurrences(arg) {
   let map = new Map()
