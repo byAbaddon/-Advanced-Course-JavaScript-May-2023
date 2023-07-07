9518ad4fc62cdf3fe91c989616245b93cb312c14
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-07-07 13:10:27 +0300

    Update 01. Reveal Words.js

--- 7.1 Text Processing - Exercise/01. Reveal Words.js
+++ 7.1 Text Processing - Exercise/01. Reveal Words.js
@@ -28,7 +28,7 @@ function revealWords(...input) {
   for (let i = 0; i < text.length; i++) {
     if (text[i].includes('*')) {
       let getLength = text[i].length
-      arrWords.split(', ').forEach(el => el.length === getLength ? text[i] = el : null)
+      arrWords.split(', ').forEach(el => el.length == getLength ? text[i] = el : null)
     }
 
     collection += text[i] + ' '
